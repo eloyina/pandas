f471c9b7d07bbd9efbca70d05ce8dfaf19764899
Elizabeth Morales Muñoz <elizabeth.morales@ucsp.edu.pe>
2022-04-16 11:42:36 -0500

    Update mse.py
    MSE en un paso pero con la prediccion:
    y= a + b x debe existir como array
    #en un solo paso
    mse= np.square(np.subtract(y,y_pred)).mean()
    print(mse)
    el otro código lo esta generando.

--- mse.py
+++ mse.py
@@ -10,14 +10,14 @@ def b_(x,y):
         sec_= (x[i]**2)
         b+=((x[i]*y[i]) - (n*(np.mean(x))*(np.mean(y)))) / (sec_ - n*third_ ) 
     formatted_b = '{0:.1f}'.format(b)
-    return formatted_b                                                       
+    return b #formatted_b                                                       
 
 
 def a_(prom_y, b, prom_x):
     a= float(prom_y) - float(b)* float(prom_x)
     ap=float('{0:.1f}'.format(a))
 
-    return ap
+    return a#ap
 
 def y_pri(a,b,x):
     n= len(x)
@@ -34,9 +34,11 @@ def y_final(y, yi):
         mse= yip/ (len(y))
     return mse
 
+
+
 x=[1,2,3,4,5]
 y=[1,1,2,2,4]
-x_pred=[0.6,1.29,1.99,2.69,3.4]
+y_pred=[0.6,1.29,1.99,2.69,3.4]
 prom_x=np.mean(x)
 print(prom_x)
 prom_y= np.mean(y)
@@ -45,3 +47,6 @@ k=float((b_(x,y)))
 a=(a_(prom_y,k, prom_x))
 yi=(y_pri(a,k,x))
 print(y_final(y,yi))
+#en un solo paso
+mse= np.square(np.subtract(y,y_pred)).mean()
+print(mse)
